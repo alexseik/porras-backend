33b8882c0f4fab1622e8dae12c8aac196b7ddf96
Alejandro Garrido <alejandro@hirodev.co>
2021-05-27 18:06:49 +0200

    update workflow

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ git pull --recurse-submodules
 
 if [ -z `docker-compose ps -q $1` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $1)` ]; then
   echo "No, it's not running."
-  docker-compose -f docker-compose.prod.yml up --build strapi
+  docker-compose -f docker-compose.prod.yml up -d --build strapi
 else
   echo "Yes, it's running."
-  docker-compose -f docker-compose.prod.yml restart strapi
+  docker-compose -f docker-compose.prod.yml restart -d strapi
 fi
 
 # End
